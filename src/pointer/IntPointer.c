cbee87a083af9fdbb26ed338567afa5d024a7bb9
linuxea <linuxea@foxmail.com>
2017-09-13 17:09:16 +0800

    * 指针定义符  & 取址操作符

--- src/pointer/IntPointer.c
+++ src/pointer/IntPointer.c
@@ -10,25 +10,25 @@
 
 void intPointer(int);
 
-int main(void){
-
-	int a = 2;
-	int b = 2;
-	int c = 3;
-
-	int d = 9999;
-
-	char chars[100];
-
-	intPointer(a);
-	intPointer(b);
-	intPointer(c);
-	intPointer(d);
-
-	printf("%p\t",&chars);
-
-	return 0;
-}
+//int main(void){
+//
+//	int a = 2;
+//	int b = 2;
+//	int c = 3;
+//
+//	int d = 9999;
+//
+//	char chars[100];
+//
+//	intPointer(a);
+//	intPointer(b);
+//	intPointer(c);
+//	intPointer(d);
+//
+//	printf("%p\t",&chars);
+//
+//	return 0;
+//}
 
 void intPointer(int i){
 	printf("%p\t",&i);
---
+++ src/pointer/IntPointerTest.c
@@ -0,0 +1,33 @@
+/*
+ * IntPointerTest.c
+ *
+ *  Created on: 2017年9月13日
+ *      Author: linuxea.lin
+ */
+
+
+#include<stdio.h>
+
+void int2Test();
+void int3Test();
+
+int main(void){
+
+	int2Test();
+	int3Test();
+	return 0;
+}
+
+void int2Test(){
+
+	int * p;
+	int a = 99;
+	p = &a;
+	printf("%p\t", p);
+}
+
+void int3Test(){
+	int a = 3;
+	int *pointer = &a; //* 指针定义符  & 取址操作符
+	printf("%p\t", pointer);
+}
