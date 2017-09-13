130a3032dcf8979e7aa74e1a7a5bbb3d3e177d7a
linuxea <linuxea@foxmail.com>
2017-09-13 12:04:11 +0800

    字符串拼接

---
+++ src/functionTest.c
@@ -0,0 +1,19 @@
+/*
+ * functionTest.c
+ *
+ *  Created on: 2017Äê9ÔÂ13ÈÕ
+ *      Author: linuxea.lin
+ */
+
+// test nested function
+
+#include<stdio.h>
+#include<string.h>
+
+int main(void){
+	char chars[80];
+	strcat(chars,"hello");
+	strcat(chars,"c programmer!");
+	printf("%s", chars);
+	return 0;
+}
