a189408fd655bd78e353daf89cedeeb3c14925df
linuxea <linuxea@foxmail.com>
2017-09-13 17:56:07 +0800

    结构体的定义与存储值

---
+++ src/String.c
@@ -0,0 +1,17 @@
+/*
+ * String.c
+ *
+ *  Created on: 2017年9月13日
+ *      Author: linuxea.lin
+ */
+
+
+#include<stdio.h>
+
+//int main(void){
+//
+//	char chs[] = "abcdefghijklmnopqrstuvwxyz"; //不要指定大小最好  以免读取到内存中一些随机值
+//	printf("%s",chs);
+//
+//	return 0;
+//}
---
+++ src/struct/Person.c
@@ -0,0 +1,29 @@
+/*
+ * Person.c
+ *
+ *  Created on: 2017年9月13日
+ *      Author: linuxea.lin
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+
+struct Person{
+	char name[20];
+	int age;
+	float score;
+};
+
+int main(void){
+
+	struct Person p1; //感觉这个struct 实现定义得有点多余
+//	p1.name = "little sun"; // 声明时可以定义 现在不行 语法规定
+	strcpy( p1.name, "little sun");
+	p1.age = 27;
+	p1.score = 89.45;
+
+	printf("name is:%s\nage is:%d\nscore is %f\n",p1.name, p1.age, p1.score);
+
+	return 0;
+}
