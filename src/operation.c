199c99e08d48941ff05b1d39ceeb49ac2d3f99aa
linuxea <linuxea@foxmail.com>
2017-09-13 11:16:14 +0800

    self plus plus

--- src/operation.c
+++ src/operation.c
@@ -9,6 +9,10 @@
 #include<stdio.h>
 
 int main(void){
+
+	int a = 1;
+	int b = 1;
+
 	printf("%d\n", 1 + 1);
 	printf("%d\n", 9 - 1);
 	printf("%d\n", 23*12);
@@ -16,6 +20,8 @@ int main(void){
 	printf("%d\n", 1<<3); //位运算符
 
 	printf("%d\n", 1^2); //
+
+	printf("%d,%d\n",a++, ++b); //自增运算符
 
 	return 0;
 }
