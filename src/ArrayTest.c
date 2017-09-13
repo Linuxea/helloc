5423d911697e6453da60c877a4b313da495ef0ba
linuxea <linuxea@foxmail.com>
2017-09-13 12:22:57 +0800

    what is array

---
+++ src/ArrayTest.c
@@ -0,0 +1,22 @@
+/*
+ * ArrayTest.c
+ *
+ *  Created on: 2017Äê9ÔÂ13ÈÕ
+ *      Author: linuxea.lin
+ */
+
+
+#include<stdio.h>
+
+int main(void){
+
+	int i = 0;
+
+	int doubleArray[5] = {1,2};
+
+	for(;i<5;i++){
+		printf("%d\t", doubleArray[i]);
+	}
+
+	return 0;
+}
