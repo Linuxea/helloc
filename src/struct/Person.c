aec47dae1a2c594377a8d07152197e0922e04388
linuxea <linuxea@foxmail.com>
2017-09-13 18:07:33 +0800

    结构指针

--- src/struct/Person.c
+++ src/struct/Person.c
@@ -15,15 +15,15 @@ struct Person{
 	float score;
 };
 
-int main(void){
-
-	struct Person p1; //感觉这个struct 实现定义得有点多余
-//	p1.name = "little sun"; // 声明时可以定义 现在不行 语法规定
-	strcpy( p1.name, "little sun");
-	p1.age = 27;
-	p1.score = 89.45;
-
-	printf("name is:%s\nage is:%d\nscore is %f\n",p1.name, p1.age, p1.score);
-
-	return 0;
-}
+//int main(void){
+//
+//	struct Person p1; //感觉这个struct 实现定义得有点多余
+////	p1.name = "little sun"; // 声明时可以定义 现在不行 语法规定
+//	strcpy( p1.name, "little sun");
+//	p1.age = 27;
+//	p1.score = 89.45;
+//
+//	printf("name is:%s\nage is:%d\nscore is %f\n",p1.name, p1.age, p1.score);
+//
+//	return 0;
+//}
---
+++ src/struct/StructPointer.c
@@ -0,0 +1,40 @@
+/*
+ * StructPointer.c
+ *
+ *  Created on: 2017年9月13日
+ *      Author: linuxea.lin
+ */
+
+
+// 结构指针
+
+
+#include<stdio.h>
+#include<string.h>
+
+struct Student{
+	char name[20];
+	int age;
+	float score;
+} stu;
+
+void structPointerTest();
+
+int main(void){
+
+	structPointerTest();
+
+	return 0;
+}
+
+
+void structPointerTest(){
+	struct Student * point = &stu; // 定义结构指针
+
+	strcpy( stu.name, "struct pointer sun");
+	stu.age = 20;
+	stu.score = 89.7;
+
+	printf("name is:%s\nage is:%d\nscore is %f\n",point->name, point->age, point->score);
+
+}
