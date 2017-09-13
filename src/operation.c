22952c925e6debc7df83fa2ddeb2760e9ca13520
linuxea <linuxea@foxmail.com>
2017-09-13 11:20:02 +0800

    递归测试

--- src/operation.c
+++ src/operation.c
@@ -8,20 +8,20 @@
 
 #include<stdio.h>
 
-int main(void){
-
-	int a = 1;
-	int b = 1;
-
-	printf("%d\n", 1 + 1);
-	printf("%d\n", 9 - 1);
-	printf("%d\n", 23*12);
-
-	printf("%d\n", 1<<3); //位运算符
-
-	printf("%d\n", 1^2); //
-
-	printf("%d,%d\n",a++, ++b); //自增运算符
-
-	return 0;
-}
+//int main(void){
+//
+//	int a = 1;
+//	int b = 1;
+//
+//	printf("%d\n", 1 + 1);
+//	printf("%d\n", 9 - 1);
+//	printf("%d\n", 23*12);
+//
+//	printf("%d\n", 1<<3); //位运算符
+//
+//	printf("%d\n", 1^2); //
+//
+//	printf("%d,%d\n",a++, ++b); //自增运算符
+//
+//	return 0;
+//}
---
+++ src/recursive.c
@@ -0,0 +1,25 @@
+/*
+ * recursive.c
+ *
+ *  Created on: 2017Äê9ÔÂ13ÈÕ
+ *      Author: linuxea.lin
+ */
+
+#include<stdio.h>
+
+int stepMulit(int);
+
+int main(void){
+
+	int result;
+	result = stepMulit(20);
+	printf("%d\n", result);
+
+	return 0;
+}
+
+int stepMulit(int i){
+	if(i == 1)
+		return 1;
+	return stepMulit(i-1) * i;
+}
