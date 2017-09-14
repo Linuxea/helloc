a8975b076835c808d68576a16d89ac86bc21de07
linuxea <linuxea@foxmail.com>
2017-09-14 14:18:37 +0800

    全部改用指针来表示节点关系

--- src/datastructure/Link.c
+++ src/datastructure/Link.c
@@ -1,10 +1,3 @@
-/*
- * Link.c
- *
- *  Created on: 2017年9月14日
- *      Author: linuxea.lin
- */
-
 
 //链表
 
@@ -16,46 +9,43 @@ struct Link{
 	struct Link * next;
 };
 
-void createLink(struct Link *);
+struct Link * createLink();
 void printLink(struct Link * );
 
 int main(void){
-	struct Link * linkHead ;
-	createLink(linkHead);
+	struct Link * linkHead = createLink();
 	printLink(linkHead);
 	return 0;
 }
 
 
-void createLink(struct Link * linkHead){
-	struct Link head;
-	strcpy(head.chs, "i am a head");
+struct Link * createLink(){
 
-	struct Link next;
-	strcpy(next.chs, "i am a head.next");
+	struct Link * head;
+	strcpy(head->chs, "i am a head");
 
-	struct Link next_next;
-	strcpy(next_next.chs, "i am a head.next_next");
+	struct Link* next;
+	strcpy(next->chs, "i am a head.next");
 
-	struct Link next_next_next;
-	strcpy(next_next_next.chs, "i am a head.next_next_next");
+	struct Link * next_next;
+	strcpy(next_next->chs, "i am a head.next_next");
+
+	struct Link * next_next_next;
+	strcpy(next_next_next->chs, "i am a head.next_next_next");
 
 	// 创建链的关系
-	head.next = &next;
-	next.next = &next_next;
-	next_next.next = &next_next_next;
-	next_next_next.next = NULL;
+	head->next = next;
+	next->next = next_next;
+	next_next->next = next_next_next;
+	next_next_next->next = NULL;
 
-	linkHead = &head; // 保存头的指针
-//	printf("%s\n", linkHead->chs);
-//	printf("%s\n", linkHead->next->chs);
-//	printf("%s\n", linkHead->next->next->chs);
-
+	return head;
 }
 
 void printLink(struct Link * temp){
 	if(NULL != temp){
 		printf("%s ->",temp->chs);
-//		printLink(temp->next);
+		temp = temp->next;
+		printLink(temp);
 	}
 }
