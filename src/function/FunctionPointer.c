42ea1787bd8d2e708dca6197e90d52e5a72ab59e
linuxea <linuxea@foxmail.com>
2017-09-15 10:14:32 +0800

    update source file name

--- src/function/FunctionPointer.c
+++ src/function/FunctionPointer.c
@@ -8,14 +8,14 @@
 #include<stdio.h>
 
 void fun(void);
-void (*functinPointer)(void);  // 声明函数指针加上()号
+void (*functonPointer)(void);  // 声明函数指针加上()号
 
-//int main(void){
-//
-//	functinPointer = &fun;
-//	functinPointer();
-//	return 0;
-//}
+int main(void){
+
+	functonPointer = &fun;
+	functonPointer();
+	return 0;
+}
 
 
 void fun(void){
