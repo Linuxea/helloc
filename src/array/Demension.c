aecd8a7f963f9897fa137b1a0e85d41451909525
linuxea <linuxea@foxmail.com>
2017-09-17 22:54:41 +0800

    二维数组与指针  ://

---
+++ src/array/Demension.c
@@ -0,0 +1,26 @@
+/*
+ * Demension.c
+ *
+ *  Created on: 2017年9月17日
+ *      Author: Administrator
+ */
+
+
+#include<stdio.h>
+
+//int main(void){
+//
+//	int array[][3] = {{1,2,3}, {11,22,33}};
+//
+//	int i , j;
+//
+//	for(i = 0;i<2;i++){
+//		for(j = 0;j<3;j++){
+//			printf("%d\t",array[i][j]);
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
---
+++ src/array/DemensionWithPointer.c
@@ -0,0 +1,17 @@
+/*
+ * DemensionWithPointer.c
+ *
+ *  Created on: 2017年9月17日
+ *      Author: Administrator
+ */
+
+#include<stdio.h>
+
+//int main(void){
+//
+//	int array[][3] = {{1,2,3}, {11,22,33}};
+//
+//	int *ap = array;
+//
+//	return 0;
+//}
