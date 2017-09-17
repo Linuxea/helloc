6e775ec6e42599a965573f2781889018220339ea
linuxea <linuxea@foxmail.com>
2017-09-18 02:52:26 +0800

    c99 for 内变量的定义

---
+++ src/c99/DefineInFor.c
@@ -0,0 +1,18 @@
+/*
+ * DefineInFor.c
+ *
+ *  Created on: 2017年9月18日
+ *      Author: Administrator
+ */
+
+
+#include<stdio.h>
+
+int main(void){
+
+	for(int i = 0;i<100;i++){
+		printf("%d\t", i);
+	}
+
+	return 0;
+}
--- src/pointer/ArrayPointer.c
+++ src/pointer/ArrayPointer.c
@@ -1,28 +1,28 @@
 /*
  * ArrayPointer.c
  *
- *  Created on: 2017年9月18日
+ *  Created on: 2017骞�9鏈�18鏃�
  *      Author: Administrator
  */
 
 
 #include<stdio.h>
 
-int main(void){
-
-	char *chs[] = {
-			"床前明月光",
-			"疑是地上霜",
-			"举头望明月",
-			"低头思故乡"
-	};
-
-	int i = 0;
-
-	for(;i<4;i++){
-		printf("%s\t",*(chs+i));
-	}
-
-	return 0;
-}
+//int main(void){
+//
+//	char *chs[] = {
+//			"搴婂墠鏄庢湀鍏�",
+//			"鐤戞槸鍦颁笂闇�",
+//			"涓惧ご鏈涙槑鏈�",
+//			"浣庡ご鎬濇晠涔�"
+//	};
+//
+//	int i = 0;
+//
+//	for(;i<4;i++){
+//		printf("%s\t",*(chs+i));
+//	}
+//
+//	return 0;
+//}
 
