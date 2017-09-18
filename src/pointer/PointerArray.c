b8c09a89ff9d55b17f666fe145c327321467b57a
linuxea <linuxea@foxmail.com>
2017-09-18 17:16:57 +0800

    反射指针的函数

--- src/pointer/PointerArray.c
+++ src/pointer/PointerArray.c
@@ -11,18 +11,18 @@
 
 #include<stdio.h>
 
-int main(void){
-
-	char *(arr[]) = {
-			"abcd",
-			"efg",
-			"ijilmn",
-			"opqrst"
-	};
-
-	for(int i = 0;i<4;i++){
-		printf("%s\t", *(arr+i));
-	}
-
-	return 0;
-}
+//int main(void){
+//
+//	char *(arr[]) = {
+//			"abcd",
+//			"efg",
+//			"ijilmn",
+//			"opqrst"
+//	};
+//
+//	for(int i = 0;i<4;i++){
+//		printf("%s\t", *(arr+i));
+//	}
+//
+//	return 0;
+//}
---
+++ src/pointer/PointerFunction.c
@@ -0,0 +1,23 @@
+/*
+ * PointerFunction.c
+ *
+ *  Created on: 2017年9月18日
+ *      Author: linuxea.lin
+ */
+
+
+// 反射指针的函数
+
+#include<stdio.h>
+
+char * hello();
+
+char * hello(){
+	return "hello";
+}
+
+int main(void){
+
+	printf("%s\n", hello());
+	return 0;
+}
