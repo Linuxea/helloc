e44fd6778916cb8b969b504096fc8829d6aa9324
linuxea <linuxea@foxmail.com>
2017-09-22 11:36:09 +0800

    冒泡排序基本版

---
+++ src/algorithm/BubbleSort.c
@@ -0,0 +1,40 @@
+/*
+ * Demension.c
+ *
+ * Author: Administrator
+ */
+
+// 冒泡排序基本版
+
+#include<stdio.h>
+
+void printArray(int *);
+
+void printArray(int *p){
+	for(int i = 0;i<10;i++){
+		printf("%d\t", *(p+i));
+	}
+	printf("\n");
+}
+
+int main(void){
+
+	int a[10] = {34,13,4,31,34,234343,43,43,44,331};
+
+	printArray(a);
+
+	for(int i = 0;i<10; i++){
+		for(int j = 0;j<10 - i - 1;j++){
+			if(a[j] > a[j+1]){
+				int temp = 0;
+				temp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = temp;
+			}
+		}
+	}
+
+	printArray(a);
+
+	return 0;
+}
