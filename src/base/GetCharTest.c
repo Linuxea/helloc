eb44833cdfdea8302cff7a9ac792928f324715b3
linuxea <linuxea@foxmail.com>
2017-09-23 23:51:19 +0800

    getchar && putchar test

--- src/base/GetCharTest.c
+++ src/base/GetCharTest.c
@@ -2,25 +2,18 @@
  * GetCharTest.c
  *
  *  Created on: 2017年9月23日
- *      Author: Administrator
+ *      Author: linuxea
  */
 
-// what is getchar
-// scanf can only read on char
-// but while true can read the chars you input one by one
-// so you can type many chars
-
-// scanf is one time !!!
 
 #include<stdio.h>
 
 int main(void){
 
-	char c;
-	while(scanf("%c", &c)){ // scanf always return 1 why? think think and you know
-		if(c == 'o')break;
-		printf("%c", c);
-	}
+	// input limit ten chars
+	int i = 0;
+	while(putchar(getchar()) && ++i<10); // will continue forever because wrap is not a invalid return value
 
 	return 0;
 }
+
---
+++ src/base/ScanfTest.c
@@ -0,0 +1,26 @@
+/*
+ * GetCharTest.c
+ *
+ *  Created on: 2017年9月23日
+ *      Author: Administrator
+ */
+
+// what is getchar
+// scanf can only read on char
+// but while true can read the chars you input one by one
+// so you can type many chars
+
+// scanf is one time !!!
+
+#include<stdio.h>
+
+//int main(void){
+//
+//	char c;
+//	while(scanf("%c", &c)){ // scanf always return 1 why? think think and you know
+//		if(c == 'o')break;
+//		printf("%c", c);
+//	}
+//
+//	return 0;
+//}
