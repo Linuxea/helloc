55fc3b1cd13a17f01ff25796f69b6c267e97c8de
linuxea <linuxea@foxmail.com>
2017-09-25 10:48:35 +0800

    dfs构造全排列

---
+++ src/algorithm/DfsDemo001.c
@@ -0,0 +1,42 @@
+/*
+ * DfsDemo001.c
+ *
+ *  Created on: 2017Äê9ÔÂ25ÈÕ
+ *      Author: linuxea.lin
+ */
+
+// dfs
+
+#include<stdio.h>
+
+int arr[] = {1,34,23,12,9};
+int used[5]; // used flag
+int result[5];
+
+void dfsPrint(int level);
+
+void dfsPrint(int level){
+
+	for(int i = 1;i<=5;i++){
+		if(used[i] == 0){
+			used[i] = 1;
+			result[level] = i;
+			dfsPrint(level+1);
+			used[i] = 0;
+		}
+	}
+
+	if(level == 5){
+		for(int i = 0;i<5;i++){
+			printf("%d->", result[i]);
+		}
+	printf("\n");
+	}
+}
+
+int main(void){
+
+	dfsPrint(0);
+
+	return 0;
+}
